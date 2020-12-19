221db04fce0ad3715995634eec4a78854ebf26a0
Christian <cunigarrod@gmail.com>
2020-12-19 15:36:10 -0500

    Page php improved.

--- page.php
+++ page.php
@@ -1,27 +1,5 @@
 <?php get_header(); ?>
 
-
-
-<!-- <div class="container mx-auto text-sm py-10 text-gray-600 px-4">
-  <p>
-    La campaña Ciudades y Pueblos por el Comercio Justo – Latinoamérica y el Caribe está conformada por personas voluntarias que conforman comités locales en cada ciudad. Participan además organizaciones de la sociedad civil y actores de los gobiernos locales. Los comités locales buscan impulsar los principios, valores, prácticas y enfoques del Comercio Justo, apoyando las iniciativas y emprendimientos de pequeños(as) productores(as) en cada ciudad o pueblo.
-  </p>
-
-  <img class="block my-10" src="https://clac-statics.s3.amazonaws.com/foto-02-pagina-quienes-somos.jpg" alt="">
-
-  <p class="mb-3">
-    Los comités locales son muy activos y realizan todo tipo de actividades para promover el Comercio Justo y la campaña y para apoyar a los pequeños productores y productoras incentivando el mercado local. Cada comité local lleva a cabo su reunión anual para la evaluación de los trabajos realizados y los resultados obtenidos. Además, elaboran un plan estratégico-operativo a nivel local para incluir las mejoras necesarias y para el cumplimiento de objetivos vinculados con los criterios de la campaña Ciudades y Pueblos por el Comercio Justo - Latinoamérica y el Caribe.
-  </p>
-
-  <p class="mb-3">
-    La campaña también cuenta con un Comité Regional, conformado por CLAC, WFTO y un representante de los comités locales. Este comité se mantiene en constante comunicación, apoyando y promocionando las ￼actividades￼ que se organizan. El primer taller regional de ciudades por el Comercio Justo de América Latina y el Caribe se celebró en el marco de la Conferencia Internacional de Ciudades por el Comercio Justo, realizada en Cardiff, Reino Unido en el 2019. En ese taller se elaboró un plan estratégico regional partiendo de las sugerencias y aportes locales.
-  </p>
-
-  <p class="mb-6">
-    Desde el 2018, la región de América Latina y el Caribe tiene representantes en el Comité Internacional de Ciudades por el Comercio Justo y desde octubre de 2019, Miriam Pérez Zelaya (del comité de Marcala, Honduras) es la representante por la región en el citado Comité Internacional.
-  </p>
-</div> -->
-
 <?php
     while(have_posts()) {
         the_post(); ?>
