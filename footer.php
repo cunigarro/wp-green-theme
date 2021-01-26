b57bee55b41bee200be9d524e964b7b18d3d7c56
Christian <cunigarrod@gmail.com>
2021-01-26 13:07:14 -0500

    Subscríbete button added.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="bg-green py-12">
       <div class="container mx-auto text-sm text-white flex items-center px-4">
         <span class="flex-1">
-          <a class="font-semibold text-base mr-4 hover:underline text-white" href="#">Suscríbete</a>
+          <!-- <a class="font-semibold text-base mr-4 hover:underline text-white" href="#">Suscríbete</a> -->
           <a class="font-semibold text-base hover:underline text-white" href="/contactanos">Contáctanos</a>
         </span>
         <div class="flex">
