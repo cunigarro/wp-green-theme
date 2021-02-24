db54a2cc25b5730931255bac667ef9cc043bacc6
Christian <cunigarrod@gmail.com>
2021-02-23 20:31:58 -0500

    Archive news section added.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -12,4 +12,18 @@
     }
 
     add_shortcode('contact_form', 'contact_form_callback');
+
+    function archived_news_callback($atts, $content = null) {
+        $atts = shortcode_atts(
+            array(),
+            $atts,
+            'archived-news'
+        );
+
+        ob_start();
+        include 'templates/archived-news.php';
+        return ob_get_clean();
+    }
+
+    add_shortcode('archived_news', 'archived_news_callback');
 ?>
